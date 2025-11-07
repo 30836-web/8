8cf4d263c6191aaf6a0cf93a6686ab78689f56ef
30836-web <30836@skpcm.ac.th>
2025-11-07 13:51:43 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,17 +2,25 @@ input.onButtonPressed(Button.A, function () {
     basic.clearScreen()
     d = -1
     a1 += d
+    a2 += d
+    a3 += d
 })
 input.onButtonPressed(Button.B, function () {
     basic.clearScreen()
-    d = 3
+    d = 1
     a1 += d
+    a2 += d
+    a3 += d
 })
 let d = 0
 basic.showNumber(507)
 let a1 = 2
+let a2 = 2
+let a3 = 2
 d = 0
 basic.forever(function () {
-    led.plot(a1, 2)
+    led.plot(3, a1)
+    led.plot(2, a2)
+    led.plot(1, a3)
     basic.pause(1000)
 })
